0ec314107b68e6f0a7086d9b844d58e61115a1c9
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-09-26 09:40:31 +1000

    Pseudocode the maze solving algorithm from hackerrank - https://www.hackerrank.com/challenges/count-luck/problem

--- GraphNode.java
+++ GraphNode.java
@@ -1,10 +1,12 @@
 public class GraphNode {
     public String name;
     public int index;
-    public boolean isVisited = false;
+    public int visitedType;
+    public String type;
 
-    public GraphNode(String name, int index) {
+    public GraphNode(String name, int index, String type) {
         this.name = name;
         this.index = index;
+        this.type = type; // M, *, ., X
     }
 }
