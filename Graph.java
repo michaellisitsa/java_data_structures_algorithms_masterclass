425aa641e616a2a597ff0f690b29ca07cc31ab91
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-09-26 09:39:03 +1000

    Add breadth first search method

--- Graph.java
+++ Graph.java
@@ -32,4 +32,35 @@ public class Graph {
         return s.toString();
     }
 
+    public ArrayList<GraphNode> getNeighbours(GraphNode node) {
+        ArrayList<GraphNode> neighbours = new ArrayList<GraphNode>();
+        int[] adjacencyRow = adjacencyMatrix[node.index];
+        for (int i = 0; i < adjacencyRow.length; i++) {
+            if (adjacencyRow[i] == 1) {
+                GraphNode selectedNode = nodeList.get(i);
+                neighbours.add(selectedNode);
+            }
+        }
+        return neighbours;
+    }
+
+    void bfsVisit(GraphNode node) {
+        // create an empty queue.
+        LinkedList<GraphNode> queue = new LinkedList<GraphNode>();
+        // Add the node passed in to the queue (starting node)
+        queue.add(node);
+        System.out.printf("Visited node:\n");
+        while (!queue.isEmpty()) {
+            GraphNode p = queue.pop();
+            if (!p.isVisited) {
+                p.isVisited = true;
+                System.out.println(p.name);
+                // read adjacent nodes;
+                ArrayList<GraphNode> neighbours = this.getNeighbours(p);
+                for (GraphNode neighbour : neighbours) {
+                    queue.add(neighbour);
+                }
+            }
+        }
+    }
 }
